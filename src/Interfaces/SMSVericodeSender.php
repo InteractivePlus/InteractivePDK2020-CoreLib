7474182091859cfc4cab935cf2c73ff7a20b8870
Windy Feng <1745500559@qq.com>
2020-07-16 11:12:02 +0800

    Update SMSVericodeSender.php

--- src/Interfaces/SMSVericodeSender.php
+++ src/Interfaces/SMSVericodeSender.php
@@ -1,8 +1,12 @@
 <?php
 namespace InteractivePlus\PDK2020Core\Interfaces;
 
+use InteractivePlus\PDK2020Core\VerificationCodes\VeriCode;
 use libphonenumber\PhoneNumber;
 
 interface SMSVericodeSender{
-    public function sendVerificationCode(string $veriCode, PhoneNumber $phoneNumber, string $toName = '', string $fromName = '');
+    public function sendVerificationCode(
+        VeriCode $code, 
+        PhoneNumber $phoneNumber
+    );
 }
